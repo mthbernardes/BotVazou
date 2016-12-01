d61dca3d732397c9a1991067cdb5fe91df236991
Matheus Bernardes <matheus.bernardes@pythonicos.com.br>
2016-12-01 21:48:46 -0200

    Update verifica.py

--- vazou/verifica.py
+++ vazou/verifica.py
@@ -14,4 +14,4 @@ class vazou():
             leaks.update(result_leakedsource)
         #print 'isleaked'
         #pprint(isleaked().check(email))
-        return set(' '.join(leaks).replace('.com','').lower().split(' '))
+        return set('*'.join(leaks).replace('.com','').lower().split('*'))
