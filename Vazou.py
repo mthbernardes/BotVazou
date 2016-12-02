2e8f572490799054f7a00d8c854da55e5ea3b7d4
Matheus Bernardes <gambler@tutanota.com>
2016-12-02 19:50:48 -0200

    change telegram def name

--- Vazou.py
+++ Vazou.py
@@ -3,9 +3,9 @@
 from bots.telegram import telegram
 from vazou.verifica import vazou
 
-def telegram():
+def bot_telegram(api,leak):
     telegram(api,leak)
 
 leak = vazou()
 telegram_api = 'YOUR-TELEGRAM-API-HERE'
-telegram(telegram_api,leak)
+bot_telegram(telegram_api,leak)
--- bots/telegram.py
+++ bots/telegram.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+
 import re
 import time
 import telepot
-
 
 class telegram(object):
     def __init__(self,api,leak):
