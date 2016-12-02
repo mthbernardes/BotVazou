038cb17acef56fcc02178f4038ea0cf6b90b3f0f
Matheus Bernardes <matheus.bernardes@pythonicos.com.br>
2016-12-02 18:52:57 -0200

    Update hackedemails.py

--- plugins/hackedemails.py
+++ plugins/hackedemails.py
@@ -3,9 +3,10 @@ from pprint import pprint
 
 class hackedemails():
     def check(self,email):
+        f_leaks = False
         url = "https://hacked-emails.com/api?q=%s" % email
         r = requests.get(url)
         leaks = r.json()
         if leaks['status'] != 'notfound':
-            leaks = [leak['title'].replace('.com','').lower() for leak in leaks['data']]
-        return leaks
+            f_leaks = [leak['title'].replace('.com','').lower() for leak in leaks['data']]
+        return f_leaks
