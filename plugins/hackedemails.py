d0edfb9e90a1259666ed3595dce4ec9a3953851a
Matheus Bernardes <gambler@tutanota.com>
2016-12-02 00:31:17 -0200

    Bugs fixed

--- plugins/hackedemails.py
+++ plugins/hackedemails.py
@@ -6,7 +6,6 @@ class hackedemails():
         url = "https://hacked-emails.com/api?q=%s" % email
         r = requests.get(url)
         leaks = r.json()
-        #pprint(leaks)
         if leaks['status'] != 'notfound':
             leaks = [leak['title'].replace('.com','').lower() for leak in leaks['data']]
         return leaks
--- vazou/verifica.py
+++ vazou/verifica.py
@@ -10,12 +10,12 @@ class vazou():
         result_haveibeenpwned = haveibeenpwned().check(email)
         result_leakedsource = leakedsource().check(email)
         result_hackedemails = hackedemails().check(email)
+        #result_isleaked = isleaked().check(email)
         if result_haveibeenpwned:
             leaks.update(result_haveibeenpwned)
         if result_leakedsource:
             leaks.update(result_leakedsource)
         if result_hackedemails:
             leaks.update(result_hackedemails)
-        #print 'isleaked'
-        #pprint(isleaked().check(email))
+            
         return set('*'.join(leaks).replace('.com','').lower().split('*'))
